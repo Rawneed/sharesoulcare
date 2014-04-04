16d8018bb68a4596c29f47065a299ab93b9618ae
Bode <bodefuwa@gmail.com>
2014-04-04 03:23:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharesoulcareOrg::Application.config.secret_key_base = '7ea8328d125b767e2d5fd53124ba8ddf6470006fe224cbf707d7e906ed616e34857a54eeb883a40edd1ed263219033fc491da9ca4472251e4f3c0426c1605d9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SharesoulcareOrg::Application.config.session_store :cookie_store, key: '_sharesoulcare_org_session'
