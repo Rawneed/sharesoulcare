191f463ef96a9a25fa66b5434568d26b7e955bd9
Bode <bodefuwa@gmail.com>
2014-06-27 07:46:27 -0400

    Fixed time format and images for events

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
+  # validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
   mount_uploader :image, ImageUploader
 #  default_scope -> { order('created_at DESC') }
   acts_as_taggable
