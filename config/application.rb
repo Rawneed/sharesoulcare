2bcba16deb59bdf788893bf7e58b8947c0f4d06c
Bode <bodefuwa@gmail.com>
2014-07-04 11:04:30 -0400

    fixed Il8n

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module ShareSoulCare
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    Il8n.enforce_available_locales = true
+#    Il8n.enforce_available_locales = true
   end
 end
